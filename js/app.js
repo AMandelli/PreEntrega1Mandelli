f5fcccc6a4b57e83afe24a4ce24cf637477b074c
AgustinMandelli <mande22rp@gmail.com>
2023-03-01 08:46:20 -0300

    Realice las mejoras pertinentes a la entrega numero uno, esto lo voy a volver a entregar

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ console.log(actualizacion);
 console.log("El monto actualizado es $" + actualizacion);
 alert("El monto actualizado es $" + actualizacion);
 
-
 let nuevoCalculo;
     
 nuevoCalculo = prompt("¿Desea relizar un nuevo calculo?");
@@ -28,6 +27,7 @@ if(nuevoCalculo == "si"){
     console.log(actualizacion);
     console.log("El monto actualizado es $" + actualizacion);
     alert("El monto actualizado es $" + actualizacion);
+    alert("Gracias por utilizar nuestros servicios!");
        
 }else if(nuevoCalculo == "no"){
     alert("Gracias por utilizar nuestros servicios!");
@@ -35,8 +35,50 @@ if(nuevoCalculo == "si"){
     alert("Funcion no disponible");
 }
 
-alert("¡Gracias por utilizar nuestros servicios!");
-console.log("¡Gracias por utilizar nuestros servicios!");
+console.log("¡Gracias por utilizar nuestros servicios!"); 
+
+// jueguemos un juego
+
+alert("Ahora te invito a que juegues conmigo... tenes que adivinar en que numero estoy pensado...");
+
+let juego = Number(prompt("Ingrese un numero del 1 al 10"));
+console.log(juego);
+
+while(juego !=7){
+    alert("El numero ingresado no es el que estoy pensando... seguí participando");
+
+    juego = Number(prompt("Ingrese un numero del 1 al 10"));
+}
+alert("Felicidades! adivinaste que numero esta en mi cabeza");
+console.log("Felicidades! adivinaste que numero esta en mi cabeza");
+console.log("Gracias por utilizar nuestro sitio web :D");
+document.write("Gracias por utilizar nuestro sitio web :D"); 
+
+// calculadora 
+
+function calculadora(primerNumero, segundoNumero, operacion){
+    switch (operacion){
+        case "+":
+            return primerNumero + segundoNumero;
+        case "-":
+            return primerNumero - segundoNumero;    
+        case "*":
+            return primerNumero * segundoNumero;
+        case "/":
+            return primerNumero / segundoNumero;
+        default:
+        return 0;        
+    }
+}
+        
+console.log(calculadora(5, 5, "+"));
+console.log(calculadora(5, 5, "-"));
+console.log(calculadora(5, 5, "*"));
+console.log(calculadora(5, 5, "/"));
+
+
+
+
 
 
 
